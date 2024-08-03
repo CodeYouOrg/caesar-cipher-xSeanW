cedaff15111d407b7e80abdaaa26e0ede1927b63
XSeanW <sean83@gmail.com>
2024-08-03 16:01:37 -0400

    cipher homework

--- cipher.py
+++ cipher.py
@@ -1,1 +1,18 @@
-# add your code here
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+shift = 5
+
+test = "python is fun!"
+
+cipher = ""
+
+for char in test.lower():
+    if char in alphabet:
+        letter = alphabet.index(char)
+        letter += shift
+        cipher += alphabet[letter % 26]
+    else: 
+            cipher = cipher + char
+
+
+print(cipher)
